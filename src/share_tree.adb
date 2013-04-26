ae652fd2829839c602de7db8023ca8b2b9e3bb6b
Ansgar Esztermann <aeszter@gwdg.de>
2013-04-26 17:09:42 +0200

    Start implementing Share Tree (Bug #1588)
    (Does not yet compile)
    -Add Navigation;
    -Add skeleton code in Viewer;
    -Add Share_Tree to do the work;
    -Expand Parser to work with non-XML data;
    -Add Spread_Sheet to store tabular data;
    
    The Spread_Sheet package is to take the same role as XMLAda's Document
    et al do for XML data.

---
+++ src/share_tree.adb
@@ -0,0 +1,47 @@
+package body Share_Tree is
+
+   --------------
+   -- Put_List --
+   --------------
+
+   procedure Put_List is
+   begin
+      --  Generated stub: replace with real body!
+      pragma Compile_Time_Warning (Standard.True, "Put_List unimplemented");
+      raise Program_Error with "Unimplemented procedure Put_List";
+   end Put_List;
+
+   -----------------
+   -- Put_Summary --
+   -----------------
+
+   procedure Put_Summary is
+   begin
+      --  Generated stub: replace with real body!
+      pragma Compile_Time_Warning (Standard.True, "Put_Summary unimplemented");
+      raise Program_Error with "Unimplemented procedure Put_Summary";
+   end Put_Summary;
+
+   -----------------
+   -- Append_List --
+   -----------------
+
+   procedure Append_List (Cells : Cell_List) is
+   begin
+      --  Generated stub: replace with real body!
+      pragma Compile_Time_Warning (Standard.True, "Append_List unimplemented");
+      raise Program_Error with "Unimplemented procedure Append_List";
+   end Append_List;
+
+   -------------
+   -- Sort_By --
+   -------------
+
+   procedure Sort_By (Field : String; Direction : String) is
+   begin
+      --  Generated stub: replace with real body!
+      pragma Compile_Time_Warning (Standard.True, "Sort_By unimplemented");
+      raise Program_Error with "Unimplemented procedure Sort_By";
+   end Sort_By;
+
+end Share_Tree;
