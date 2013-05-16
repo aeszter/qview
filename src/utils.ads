7ecb8507b310c1e5735488c94366355b6631a20a
Ansgar Esztermann <aeszter@gwdg.de>
2013-05-16 11:00:39 +0200

    Bump version number

--- src/utils.ads
+++ src/utils.ads
@@ -5,7 +5,7 @@ with Ada.Containers.Ordered_Sets;
 with POSIX; use POSIX;
 
 package Utils is
-   Version : String := "1.1d"; -- Update Bugzilla when you change this
+   Version : String := "1.2d"; -- Update Bugzilla when you change this
    type Tri_State is (False, True, Undecided);
    Assumption_Error : exception;
 
