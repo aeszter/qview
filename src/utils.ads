2cd42876ad944e19a91b4ea7d0daf876ce7e4a38
Ansgar Esztermann <aeszter@gwdg.de>
2013-09-12 10:49:19 +0200

    version bump

--- src/utils.ads
+++ src/utils.ads
@@ -5,7 +5,7 @@ with Ada.Containers.Ordered_Sets;
 with POSIX; use POSIX;
 
 package Utils is
-   Version : String := "feature:qstat-j"; -- Update Bugzilla when you change this
+   Version : String := "develop"; -- Update Bugzilla when you change this
    type Tri_State is (False, True, Undecided);
    Assumption_Error : exception;
 
