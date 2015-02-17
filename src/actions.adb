81a08d81c73e1d330f8ab6fd1abd0cbfabee3c22
Ansgar Esztermann <aeszter@gwdg.de>
2015-02-17 15:43:05 +0100

    Action mode (Bug #2237): fix redirection
    Status line is generated by server, we use “Location:” only.

--- src/actions.adb
+++ src/actions.adb
@@ -17,8 +17,7 @@ package body Actions is
       if What = "cj" then
          SGE.Actions.Clear_Error (The_Job => Integer'Value (HTML.Param ("j")));
          if Referrer /= "" then
-            Ada.Text_IO.Put_Line ("HTTP/1.1 301 Moved Permanently");
-            Ada.Text_IO.Put_Line ("Location: " & Referrer);
+            CGI.Put_CGI_Header ("Location: " & Referrer);
          else
             Viewer.Put_Result ("OK");
          end if;
