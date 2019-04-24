ff89bfaa4c1f1f56c71394325b19afa57dec46e4
Ansgar Esztermann <aeszter@gwdg.de>
2019-04-24 11:27:31 +0200

    post-release version bump

--- src/utils.ads
+++ src/utils.ads
@@ -1,6 +1,6 @@
 
 package Utils is
-   Version : String := "3.14"; -- Update Bugzilla when you change this
+   Version : String := "pre3.15"; -- Update Bugzilla when you change this
 
    procedure Read_Link (Path : String; Buffer : out String; Last : out Natural);
 
