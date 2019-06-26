c0761d42c54a204e29e3b197fbf4877e94d90066
Ansgar Esztermann <aeszter@gwdg.de>
2019-06-26 09:29:19 +0200

    add exception user_error

--- src/utils.ads
+++ src/utils.ads
@@ -2,6 +2,8 @@
 package Utils is
    Version : String := "develop"; -- Update Bugzilla when you change this
 
+   User_Error : exception;
+
    procedure Read_Link (Path : String; Buffer : out String; Last : out Natural);
    function To_String (Source : Integer) return String;
 
