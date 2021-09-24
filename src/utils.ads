cc91a3fc0f8f671f2331de17b36f69936e3d55d2
Ansgar Esztermann <aeszter@gwdg.de>
2021-09-24 10:53:05 +0200

    pre-release version bump

--- src/utils.ads
+++ src/utils.ads
@@ -1,6 +1,6 @@
 
 package Utils is
-   Version : String := "3.25"; -- Update Bugzilla when you change this
+   Version : String := "3.26"; -- Update Bugzilla when you change this
 
    procedure Read_Link (Path : String; Buffer : out String; Last : out Natural);
    function To_String (Source : Integer) return String;
